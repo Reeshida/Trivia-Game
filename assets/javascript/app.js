97be0ae25d594bcfe6ee19f5c66c95578d5c87d2
Reeshida <reeshida@hotmail.com>
2018-06-06 20:56:13 -0400

    added css style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,7 +62,7 @@ var game ={
     start: function(){
         timer=setInterval(game.countdown,1000);
         $("#subwrapper").prepend('<h2> Time Remaining: <span id="counter">120/span>Seconds</h2>');
-        // $('#start').on('click',function() {
+        
          $('#start').remove();
          for(var i=0; i<questions.length; i++){
             $('#subwrapper').append('<h2>' + questions[i].question +'</h2>');
@@ -72,7 +72,7 @@ var game ={
         }
 
         $("#subwrapper").append("<br><button id ='submitButton'>Click to Submit</button>")
-    // })
+    
     },
     done: function(){
          $.each($('input[name=question-0]:checked'),function(){
